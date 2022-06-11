deb856ec5056efd67eb902cfdb54d15aad8c259c
unknown <fatma.elarabii@gmail.com>
2022-06-11 02:58:02 -0700

    put components in seperate files

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from './logo.svg';
 import './App.css';
 
-function NavbarMenu() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand 
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-            <img src={logo} className="App-logo" alt="logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-  )
-}
-
-function MainContent() {
-  return (
-    <Container>
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </Container>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2022 Elarabi development. All rights reserved.</small>
-    </footer>
-  )
-}
+import NavbarMenu from './components/Navbar'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   return (
